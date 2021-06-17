0a761ce80e24a525c22fbe3613d92598a500449e
Michael Frank <frank.522@osu.edu>
2021-06-17 16:02:11 -0400

    implemented basic UI with functionality

--- main.js
+++ main.js
@@ -1,59 +1,114 @@
-function computerPlay()  {
+const wins = document.querySelector('#wins');
+const losses = document.querySelector('#losses');
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const startBtn = document.querySelector('#start');
+const outputdiv = document.querySelector('.results');
+const resultNode = document.createElement('p');
+
+let startFunction = (e) => {
+    if(e.target.innerText == 'start') {
+        startGame();
+    }
+};
+
+startBtn.addEventListener('click', startFunction);
+
+let computerPlay = () => {
     let value = 3 * Math.random();
     if(value < 1) {
-        return "scissors";
+        return 'scissors';
     } else if(value < 2){
-        return "rock";
+        return 'rock';
     } else{
-        return "paper";
+        return 'paper';
     }
-}
+};
 
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    console.log(playerSelection);
-    console.log(computerSelection);
+let playRound = (playerSelection, computerSelection) => {
     switch(playerSelection){
         case "scissors":
             switch(computerSelection) {
                 case "scissors":
-                    return "Tie, " + playerSelection + " ties with " + computerSelection;
-                case "sock":
-                    return "You Lose, " + computerSelection + " beats " + playerSelection;
+                    return "It's a tie";
+                case "rock":
+                    return "You lose";
                 case "paper":
-                    return "You Win, " + playerSelection + " beats " + computerSelection;
+                    return "You win";
             }
         case "rock":
             switch(computerSelection) {
                 case "rock":
-                    return "Tie, " + playerSelection + " ties with " + computerSelection;
+                    return "It's a tie";
                 case "paper":
-                    return "You Lose, " + computerSelection + " beats " + playerSelection;
+                    return "You lose";
                 case "scissors":
-                    return "You Win, " + playerSelection + " beats " + computerSelection;
+                    return "You win";
             }
         case "paper":
             switch(computerSelection) {
                 case "paper":
-                    return "Tie, " + playerSelection + " ties with " + computerSelection;
+                    return "It's a tie";
                 case "scissors":
-                    return "You Lose, " + computerSelection + " beats " + playerSelection;
+                    return "You lose";
                 case "rock":
-                    return "You Win, " + playerSelection + " beats " + computerSelection;
+                    return "You win";
             }
     }
-}
+};
 
+let rpsPress = (e) => {
+    let computerSelection = computerPlay();
+    let playerSelection = e.target.innerText;
+    let result = playRound(playerSelection, computerSelection);
+    let resultString = "You chose " + playerSelection + ", Opponent chose " + computerSelection + "! " + result + "!";
+    resultNode.innerText = resultString;
+    outputdiv.appendChild(resultNode);
+    updateGame(result);
+};
 
+function startGame(){
+    window.removeEventListener('click', startFunction);
+    wins.innerText = 0;
+    losses.innerText = 0;
+    rockBtn.addEventListener('click', rpsPress);
+    paperBtn.addEventListener('click', rpsPress);
+    scissorsBtn.addEventListener('click', rpsPress);
+};
 
-
-function PlayGame(){
-    for(let i=0; i<5; i++){
-        let playerChoice = prompt("Rock, Paper or Scissors: ");
-        let computerChoice = computerPlay();
-        console.log(playRound(playerChoice, computerChoice));
+let updateGame = (result) => {
+    switch(result){
+        case "You win":
+            wins.innerText = +wins.innerText + 1;
+            break;   
+        case "It's a tie":
+            break;
+        case "You lose":
+            losses.innerText = +losses.innerText + 1;
+            break;
     }
-}
 
-PlayGame();
+    if(wins.innerText == 5 || losses.innerText == 5) {
+        endGame();
+    }
+};
+
+let endGame = () => {
+    rockBtn.removeEventListener('click', rpsPress);
+    paperBtn.removeEventListener('click', rpsPress);
+    scissorsBtn.removeEventListener('click', rpsPress);
+    startBtn.addEventListener('click', startFunction);
+
+    if(wins.innerText == 5){
+        resultNode.innerText += "\n Congratulations, you have beaten the computer!";
+        outputdiv.appendChild(resultNode);
+    } else {
+        resultNode.innerText += "\n Sorry, you have been beaten by the computer!";
+        outputdiv.appendChild(resultNode);
+    }
+};
+
+
+
+
